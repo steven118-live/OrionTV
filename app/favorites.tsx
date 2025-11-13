e8144629dae2b0fd622fd7a6cfc7988418d6206e
Steven118 <steven118@live.com>
2025-11-13 22:00:23 +0800

    fix: update Logger imports to @/debug/logger in modified files

--- app/favorites.tsx
+++ app/favorites.tsx
@@ -11,7 +11,7 @@ import { useResponsiveLayout } from "@/hooks/useResponsiveLayout";
 import { getCommonResponsiveStyles } from "@/utils/ResponsiveStyles";
 import ResponsiveNavigation from "@/components/navigation/ResponsiveNavigation";
 import ResponsiveHeader from "@/components/navigation/ResponsiveHeader";
-import Logger from "@/debug/logger";
+import Logger from "../src/debug/logger";
 import { logLineShort } from "@/debug/utils/devLog";
 
 
